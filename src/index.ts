5b12806e69359d8d55280127235abc195f089518
Jan Piotrowski <piotrowski+github@gmail.com>
2023-09-21 10:36:03 +0000

    add console.* polyfills

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import 'react-native-polyfill';
 // import 'core-js/actual/url';
 import "core-js";
 
+console.warn = (e) => {console.log(e)};
+console.info = (i) => {console.log(i)};
+console.error = (e) => {console.log(e)};
+console.trace = (...t) => {console.log(t)};
+
 import { PrismaClient } from '@prisma/client/edge.js';
 
 class TestCase {
